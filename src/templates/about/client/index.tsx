e3cc5b9f106c4337e6ea28c3a2352facca43e226
Austin <grayaustinc@gmail.com>
2022-04-18 01:29:36 -0400

    about cleanup

--- src/templates/about/client/index.tsx
+++ src/templates/about/client/index.tsx
@@ -47,69 +47,67 @@ const AboutPage: NextPage<PageProps> = ({ locations }) => {
       <div className={style["bg"]}>
         <Container className="my-2">
           <Row className="my-5">
-            <Col md={4} className="mt-md-5 mb-auto text-center">
+            <Col lg={4} className="mt-lg-5 mb-auto text-center">
               <Card.Body className={style["image-container"]}>
                 <NextImage src={stacy} sizes={sizes} quality={75} placeholder="blur" objectFit="contain" />
               </Card.Body>
             </Col>
-            <Col md={8}>
-              <Card.Body>
-                <h2 className={style["h"]}>Stacy Gray</h2>
-                <div className={style["phone"]}>
-                  <Link href="tel:+1-919-810-3743" passHref>
-                    <a>(919) 810-3743</a>
-                  </Link>
-                </div>
-                <Card.Text className={`${style["b"]} mb-4`}>
-                  I am a Raleigh, NC based travel agent specializing in planning honeymoons, family gatherings, group travel and destination weddings. My passion is to help you
-                  find and experience the most amazing dream vacations. I take the stress out of planning by taking care of all the details so you can relax and enjoy your escape
-                  vacation! I love creating tailor-made vacations and my valued clients get my first hand experience.
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  My background is in healthcare, and I am a chiropractor in Raleigh NC for the last 26 years and a travel agent for the last 9 years. I launched Adjust to the Good
-                  Life Travel in 2013 based solely on my love and passion for travel! My dad was a Naval officer and we traveled extensively growing up when we lived in Europe. Our
-                  family loves exploring new destinations, experiencing unique adventures, and creating lifelong memories together. My daughter, Haley Gray has joined the family
-                  business and is now also a travel consultant with our travel agency.
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  Our family has cruised over 25 different sailings to almost all of the Caribbean islands, Hawaii, Alaska, Europe, Tahiti, Galapagos and other exotic destinations.
-                  We have explored over 33 countries, and traveled to almost all of the states within the U.S. We also love exploring the U.S National parks and taking expeditions.
-                  As an experienced travel partner, I have the personal knowledge to help your family too! Having the personal knowledge of these destinations has enabled me to see
-                  first hand what my clients will experience.
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  I hold one of the highest levels of certification within the cruise industry, as a Master Cruise Counselor designated through CLIA (Cruise Lines International
-                  Association). It is a rigorous program that includes first attaining Accredited Cruise Counselor status, after selling many, many cruises, inspecting and
-                  personally sailing on different types of ships. This program enabled me to experience a wide range of cruise brands and gain first hand knowledge so that I can
-                  create the perfect experience for my clients.
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  As a preferred Sandals and Beaches Specialist, I have completed an extensive educational program where I learned and experienced first hand about the Sandals and
-                  Beaches resorts. Being recognized by the industry as a Sandals expert, enables me to help match you to the perfect Caribbean destination.
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  Thank you for taking the time to visit Adjust to the Good Life Travel and considering us for any and all of your vacation planning needs. We are truly grateful
-                  for the opportunity to work with you. I love helping my clients discover new and exciting adventures and enjoy building lasting relationships!
-                </Card.Text>
-                <Card.Text className={`${style["b"]} mt-4`}>
-                  Feel free to sign up for a{" "}
-                  <Link href="https://10to8.com/book/tjfpqu-free/" passHref>
-                    <a className={blackLinkClassName} rel="nofollow noindex" target="_blank">
-                      Dream Vacation Planning Session
-                    </a>
-                  </Link>{" "}
-                  so I can help you turn your travel dreams into memorable vacations!
-                </Card.Text>
-              </Card.Body>
+            <Col lg={8}>
+              <h2 className={style["h"]}>Stacy Gray</h2>
+              <div className={style["phone"]}>
+                <Link href="tel:+1-919-810-3743" passHref>
+                  <a>(919) 810-3743</a>
+                </Link>
+              </div>
+              <Card.Text className={`${style["b"]} mb-4`}>
+                I am a Raleigh, NC based travel agent specializing in planning honeymoons, family gatherings, group travel and destination weddings. My passion is to help you find
+                and experience the most amazing dream vacations. I take the stress out of planning by taking care of all the details so you can relax and enjoy your escape
+                vacation! I love creating tailor-made vacations and my valued clients get my first hand experience.
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                My background is in healthcare, and I am a chiropractor in Raleigh NC for the last 26 years and a travel agent for the last 9 years. I launched Adjust to the Good
+                Life Travel in 2013 based solely on my love and passion for travel! My dad was a Naval officer and we traveled extensively growing up when we lived in Europe. Our
+                family loves exploring new destinations, experiencing unique adventures, and creating lifelong memories together. My daughter, Haley Gray has joined the family
+                business and is now also a travel consultant with our travel agency.
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                Our family has cruised over 25 different sailings to almost all of the Caribbean islands, Hawaii, Alaska, Europe, Tahiti, Galapagos and other exotic destinations.
+                We have explored over 33 countries, and traveled to almost all of the states within the U.S. We also love exploring the U.S National parks and taking expeditions.
+                As an experienced travel partner, I have the personal knowledge to help your family too! Having the personal knowledge of these destinations has enabled me to see
+                first hand what my clients will experience.
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                I hold one of the highest levels of certification within the cruise industry, as a Master Cruise Counselor designated through CLIA (Cruise Lines International
+                Association). It is a rigorous program that includes first attaining Accredited Cruise Counselor status, after selling many, many cruises, inspecting and personally
+                sailing on different types of ships. This program enabled me to experience a wide range of cruise brands and gain first hand knowledge so that I can create the
+                perfect experience for my clients.
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                As a preferred Sandals and Beaches Specialist, I have completed an extensive educational program where I learned and experienced first hand about the Sandals and
+                Beaches resorts. Being recognized by the industry as a Sandals expert, enables me to help match you to the perfect Caribbean destination.
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                Thank you for taking the time to visit Adjust to the Good Life Travel and considering us for any and all of your vacation planning needs. We are truly grateful for
+                the opportunity to work with you. I love helping my clients discover new and exciting adventures and enjoy building lasting relationships!
+              </Card.Text>
+              <Card.Text className={`${style["b"]} mt-4`}>
+                Feel free to sign up for a{" "}
+                <Link href="https://10to8.com/book/tjfpqu-free/" passHref>
+                  <a className={blackLinkClassName} rel="nofollow noindex" target="_blank">
+                    Dream Vacation Planning Session
+                  </a>
+                </Link>{" "}
+                so I can help you turn your travel dreams into memorable vacations!
+              </Card.Text>
             </Col>
           </Row>
           <Row className="my-5">
-            <Col md={{ span: 4, order: "last" }} className="mt-md-5 mb-auto text-center">
+            <Col lg={{ span: 4, order: "last" }} className="mt-lg-5 mb-auto text-center">
               <Card.Body className={style["image-container"]}>
                 <NextImage src={haley} sizes={sizes} quality={75} placeholder="blur" objectFit="contain" />
               </Card.Body>
             </Col>
-            <Col md={{ span: 8, order: "first" }}>
+            <Col lg={{ span: 8, order: "first" }}>
               <Card.Body>
                 <h2 className={style["h"]}>Haley Gray</h2>
                 <div className={style["phone"]}>
